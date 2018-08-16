61579ad59f27859f0e8dd0c2630b2cdd5153ba75
Spencer Wallace <swallace@weddingstar.com>
2018-08-16 17:17:42 -0600

    The Basic auth token request is working

--- src/actions/login.js
+++ src/actions/login.js
@@ -1,5 +1,16 @@
 import axios from 'axios';
 
 export function login(data) {
-    return axios.post('/api/auth', data);
+    const accessData = {
+        grant_type: 'password',
+        client_id: 2,
+        client_secret: 'WbnTIzMtpAK4xsYtg9Kmt9sgmdd94tkg7o85jQWA',
+        username: 'spencer@wallace.com',
+        password: 'password',
+        scope: '',
+    }
+    const requestData = Object.assign(data, accessData);
+
+    console.log(requestData);
+    return axios.post('http://192.168.10.20/oauth/token', requestData);
 }
