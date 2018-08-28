57270807e57669728568e4d8ac75b3ab38f81b22
Spencer Wallace <dude.wallace@gmail.com>
2018-08-28 17:13:58 -0600

    Add users request to Admin page

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,7 +1,39 @@
 import React, { Component } from "react";
 import { AuthConsumer } from "./AuthContext";
+import axios from 'axios';
 
 class Admin extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      users:  {}
+    }
+
+  }
+
+  componentDidMount() {
+    const header = {
+      'Authorization': 'Bearer ' + localStorage.getItem('access_token'),
+      'Accept': 'application/json'
+    };
+
+    axios.get('http://192.168.10.20/api/users', {headers: header})
+      .then(
+        response => {
+          this.setState({
+            users: response.data.data,
+            first: response.data.links.first,
+            last: response.data.links.last,
+            next: response.data.links.next,
+            prev: response.data.links.prev
+          });
+          console.log('first', this.state.first); 
+        },
+        error => console.log(error)
+      )
+  }
+
   render() {
     return (
       <AuthConsumer>
