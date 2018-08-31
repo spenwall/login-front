e4e0386f68a4e600f15a6ccc84c8fb18694df2be
spenwall <dude.wallace@gmail.com>
2018-08-30 19:37:07 -0600

    Add users to admin page

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,14 +1,68 @@
 import React, { Component } from "react";
 import { AuthConsumer } from "./AuthContext";
+import axios from 'axios';
 
 class Admin extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      users:  [],
+      first: '',
+      next: '',
+      last: '',
+      prev: '',
+    }
+
+  }
+
+  componentDidMount() {
+    const header = {
+      'Authorization': 'Bearer ' + localStorage.getItem('access_token'),
+      'Accept': 'application/json'
+    };
+
+    axios.get('http://192.168.10.20/api/users', {headers: header})
+      .then(
+        response => {
+          this.setState({
+            users: response.data.data,
+            first: response.data.links.first,
+            last: response.data.links.last,
+            next: response.data.links.next,
+            prev: response.data.links.prev
+          });
+          console.log('first', this.state.first); 
+        },
+        error => console.log(error)
+      )
+  }
+
   render() {
+    const users = this.state.users 
+    ? this.state.users.map((user, i) => {
+     return <div key={user.id}>
+              <div className="card">
+                <div className="card-header">
+                  <div className="card-header-title">
+                    {user.name}
+                  </div>
+                </div>
+                <div className="card-content">
+                  {user.email}
+                  {user.admin}
+                </div>
+              </div>
+            </div>
+    }) 
+    : '';
+
     return (
       <AuthConsumer>
         { (context) => (
-          context.state.user.admin === '1'
-          ? <div>You are admin</div>
-          : <div>You are NOT admin</div>
+          context.state.user.admin === 'admin'
+          ? <div>You are NOT admin</div>
+          : users
         )}
       </AuthConsumer>
     );
