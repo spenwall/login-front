bf3bd2e9fe38a889f3e1e4aeabd70b5bcc0d1cb2
Spencer <swallace@weddingstar.com>
2018-09-21 10:35:14 -0600

    Update passportAddress to include https

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const passportAddress = 'login.spencerwallace.ca';
+export const passportAddress = 'https://login.spencerwallace.ca';
 export const clientSecret = 'dmCXyvB0KMgyMvlwGo3ci4j579AA82uyZa5qL6LO';
